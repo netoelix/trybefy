c157c4f6e14209dd1c266942b0dcda294d5d04b2
Elix Neto <neto.elix@outlook.com>
2024-01-22 19:50:18 -0300

    Finalizando requisito 7

--- src/06_challenge.sql
+++ src/06_challenge.sql
@@ -1,11 +1,11 @@
 UPDATE users
 SET active = TRUE
-WHERE id = 4
+WHERE id = 4;
 
 UPDATE users
 SET active = FALSE
-WHERE id = 8
+WHERE id = 8;
 
 UPDATE users
 SET plan_id = 2
-WHERE id = 2
+WHERE id = 2;
