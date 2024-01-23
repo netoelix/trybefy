9096623d12913b99c4e90ca5c41ed28081c8c369
Elix Neto <neto.elix@outlook.com>
2024-01-22 22:14:41 -0300

    Finalizando requisito 19

---
+++ src/19_challenge.sql
@@ -0,0 +1,6 @@
+SELECT son.title AS 'Título da música', son.duration_in_seconds AS 'Duração', al.title AS 'Álbum'
+FROM songs AS son
+INNER JOIN albums AS al
+ON son.album_id = al.id
+WHERE son.duration_in_seconds BETWEEN 300 AND 480
+ORDER BY `Duração` ASC;
