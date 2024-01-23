05c38cb13fff23ed89b6aebf0c7121e61e9ffc95
Elix Neto <neto.elix@outlook.com>
2024-01-22 22:43:00 -0300

    Fix: req 30

--- src/30_challenge.sql
+++ src/30_challenge.sql
@@ -1,5 +1,5 @@
-SELECT ar.name AS 'Artista', COUNT(af.user_id) AS 'Total de Seguidores'
+SELECT ar.name AS 'Artista', COUNT(af.user_id) AS 'Total de seguidores'
 FROM artists AS ar
 INNER JOIN artists_followers AS af ON ar.id = af.artist_id
 GROUP BY ar.name
-HAVING `Total de Seguidores` < 5;
+HAVING `Total de seguidores` < 5;
